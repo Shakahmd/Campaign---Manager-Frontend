90d46e704d881d40bbbe6f24c0b23be60640c968
shakahmd <shakkirahammad2002@gmail.com>
2024-08-15 00:30:30 +0530

    Removed the baseUrl.replace("/api", '') logic from the Download.jsx component, simplifying URL handling

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -8,7 +8,7 @@ import { useLocation, useNavigate } from 'react-router-dom'
 
 const Download = () => {
   let baseURL = import.meta.env.VITE_API_BASE_URL
-   baseURL.replace('/api','')
+  
   const navigate = useNavigate()
   const location  = useLocation()
    const baseUrl = `${baseURL}/generated/`
